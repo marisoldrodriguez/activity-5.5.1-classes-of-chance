c69da0ccaaa3ede4d883059000a2d3b14bf726dc
Marisol Rodriguez <maridrodriguez@gmail.com>
2022-03-23 20:19:04 -0400

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
 class Casino {
-    // Write code here
+    constructor(name, fake){
+        this.name = name;
+        this.timesPlayed = 0;
+        this.isFakeCoin = fake;
+    }
+    playGame(betAmount){
+        this.timesPlayed += 1
+        let earnings = betAmount * (this.timesPlayed + 1)
+        if (this.isFakeCoin === true){
+            console.log(this.name + " " + "wins!")
+        } else if (Math.random() <= .5){
+            console.log(this.name + " " + "wins!")
+        } else {
+            console.log(`player wins ${earnings}`)
+        }
+    }  
 };
 
 // TESTS
-const myCasino = new Casino("HackerU Casino");
+const myCasino = new Casino("HackerU Casino", false);
+
 console.log(myCasino);
 myCasino.playGame(5);
 myCasino.playGame(15);
@@ -11,20 +27,11 @@ myCasino.playGame(25);
 myCasino.playGame(35);
 
 // BONUS TESTS
-/*
+
 const myBonusCasino = new Casino("HackerU Bonus Casino", true);
 console.log(myBonusCasino);
 myBonusCasino.playGame(5);
 myBonusCasino.playGame(15);
 myBonusCasino.playGame(25);
 myBonusCasino.playGame(35);
-*/
 
-// Extra BONUS TESTS
-/*
-const myExtraBonusCasino = new Casino("HackerU Extra Bonus Casino", false);
-console.log(myExtraBonusCasino);
-myExtraBonusCasino.rollDie(6);
-myExtraBonusCasino.rollDie(20);
-myExtraBonusCasino.rollDie(100);
-*/
